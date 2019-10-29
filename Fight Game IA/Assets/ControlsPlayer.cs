c90884317b77e1966d41d0ce1e34ba9d9d4197d7
miguelangelgil <miguel.gilmartin12@gmail.com>
2019-10-29 17:33:11 +0100

    Controls player and component jump

---
+++ Fight Game IA/Assets/ControlsPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsPlayer : MonoBehaviour
+{
+    public KeyCode atack1;
+    public KeyCode atack2;
+    public KeyCode atack3;
+    public KeyCode atack4;
+    public KeyCode jump;
+    public KeyCode crouch;
+    private Jump myJumper;
+    private bool atacking;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(GetComponent<Jump>())
+            myJumper = GetComponent<Jump>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Acciones de movimiento vertical
+        if (Input.GetKeyDown(jump)) { myJumper.JumpOwner(); }
+        else if (Input.GetKeyDown(crouch)) {/*TODO*/ }
+        else {/*TODO*/ }
+
+        //Acciones de ataque
+        if (Input.GetKeyDown(atack1)) { GetComponent<SpriteRenderer>().color = Color.red; }
+        else if (Input.GetKeyDown(atack2)) { GetComponent<SpriteRenderer>().color = Color.green; }
+        else if (Input.GetKeyDown(atack3)) { GetComponent<SpriteRenderer>().color = Color.yellow; }
+        else if (Input.GetKeyDown(atack4)) { GetComponent<SpriteRenderer>().color = Color.blue; }
+        else { GetComponent<SpriteRenderer>().color = Color.white; }
+
+    }
+}
---
+++ Fight Game IA/Assets/Jump.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    private Transform myTransform;
+    //Posicion normal de pelea
+    private Vector2 initialPosition;
+    //Posicion mas alta del salto
+    private Vector2 endPosition;
+    //Altura maxima
+    public float height;
+    //Tiempo que tarda en realizar la acción
+    public float timeAction;
+    //Variable auxiliar para realizar la interpolacion
+    private float auxTime=0;
+    // Start is called before the first frame update
+    private bool jumping;
+    void Start()
+    {
+        jumping = false;
+        myTransform = GetComponent<Transform>();
+        initialPosition = myTransform.position;
+        endPosition = new Vector2(myTransform.position.x, myTransform.position.y + height);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void JumpOwner()
+    {
+        jumping = true;
+        while (auxTime <= timeAction)
+        {
+            myTransform.position = Vector2.Lerp(initialPosition, endPosition, auxTime);
+            auxTime += Time.deltaTime; 
+        }
+    }
+    public void FallOwner()
+    {
+        while (auxTime >= 0)
+        {
+            myTransform.position = Vector2.Lerp(initialPosition, endPosition, auxTime);
+            auxTime -= Time.deltaTime;
+        }
+        jumping = false;
+    }
+    public bool GetJumping()
+    {
+        return jumping;
+    }
+}
